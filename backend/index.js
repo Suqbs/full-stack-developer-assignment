b68b6f13484593c258e87f470d24558ca7169ce2
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2025-07-05 23:58:04 +0300

    Add GET request and its functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 const app = express();
 const PORT = 3000;
@@ -10,28 +11,54 @@ app.use(cors());
 console.log('Sunucu başladı');
 
 function GetGoldPrice() {
-    return fetch("https://kapalicarsi.apiluna.org/aa")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('There is a problem with the fetch: ' + response.statusText);
-        }
+    return fetch("https://kapalicarsi.apiluna.org/")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('There is a problem with the fetch: ' + response.statusText);
+            }
 
-        return response.json();
-    })
-    .then(data => {
-        const GOLDTRY = data.find(item => item.code === 'ALTIN').satis;
-        const USDTRY = data.find(item => item.code === 'USDTRY').satis;
+            return response.json();
+        })
+        .then(data => {
+            const GOLDTRY = data.find(item => item.code === 'ALTIN').satis;
+            const USDTRY = data.find(item => item.code === 'USDTRY').satis;
 
-        const GOLDUSD = GOLDTRY / USDTRY;
-        console.log('Altın fiyatı (USD):', GOLDUSD);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+            const GOLDUSD = GOLDTRY / USDTRY;
+            return GOLDUSD;
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-GetGoldPrice();
+app.get('/api/products', async (req, res) => {
+    try {
+        const goldPrice = await GetGoldPrice();
+
+        // This line doesnt work
+        const productsData = fs.readFileSync('products.json', {encoding: "utf8"});
+        const products = JSON.parse(productsData);
+
+        const productsWithPrice = products.map(product => {
+            const price = (product.popularityScore + 1) * product.weight * goldPrice;
+
+            return {
+                ...product,
+                price: parseFloat(price.toFixed(2))
+            }
+        })
+
+        console.log("--- PRODUCT DATA ---");
+        console.log(productsWithPrice);
+        console.log("--------------------");
+
+        res.json(productsWithPrice);
+    }
+    catch (error) {
+        console.log("Error fetching products");
+    }
+});
 
 app.listen(PORT, () => {
-  console.log(`API sunucusunun adresi: http://localhost:${PORT}`);
+    console.log(`API sunucusunun adresi: http://localhost:${PORT}`);
 });
