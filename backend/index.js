ba1832b9e956e03b47e3f84b69021b415ecbcef2
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2025-07-05 20:10:06 +0300

    Create the function that gets gold price

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import fs from 'fs';
 import cors from 'cors';
 import fetch from 'node-fetch';
 
@@ -7,3 +6,32 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+console.log('Sunucu başladı');
+
+function GetGoldPrice() {
+    return fetch("https://kapalicarsi.apiluna.org/aa")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('There is a problem with the fetch: ' + response.statusText);
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        const GOLDTRY = data.find(item => item.code === 'ALTIN').satis;
+        const USDTRY = data.find(item => item.code === 'USDTRY').satis;
+
+        const GOLDUSD = GOLDTRY / USDTRY;
+        console.log('Altın fiyatı (USD):', GOLDUSD);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+GetGoldPrice();
+
+app.listen(PORT, () => {
+  console.log(`API sunucusunun adresi: http://localhost:${PORT}`);
+});
