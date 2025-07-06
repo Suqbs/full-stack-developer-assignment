a9fcd7db26263b19e9901a65b5047643ba58b501
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2025-07-06 15:15:28 +0300

    Add functionality to carousel

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,49 @@
-const score = 2.8;
+document.addEventListener('DOMContentLoaded', () => {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const carouselTrack = document.querySelector('.carousel-track');
+    const prevButton = document.querySelector('.carousel-button.prev');
+    const nextButton = document.querySelector('.carousel-button.next');
+    const productCards = document.querySelectorAll('.product-card');
 
-const starsContainer = document.querySelector('.stars');
+    if (productCards.length > 0) {
+        const cardGap = parseFloat(window.getComputedStyle(carouselTrack).gap);
+        const cardWidth = productCards[0].offsetWidth;
 
-for (let i = 0; i < 5; i++) {
-    let fillPercentage = 0;
+        nextButton.addEventListener('click', () => {
+            const currentScroll = carouselContainer.scrollLeft;
+            let targetScroll = 0;
 
-    if (score >= i)
-        fillPercentage = 100;
-    else if (score > i - 1)
-        fillPercentage = (score - (i - 1)) * 100;
-}
+            for(const card of productCards) {
+                if(card.offsetLeft > currentScroll + (cardWidth / 2)) {
+                    targetScroll = card.offsetLeft;
+                    console.log("Gidilen konum", targetScroll);
+                    break;
+                }
+            }
+
+            carouselContainer.scrollTo({
+                left: targetScroll,
+                behavior: 'smooth'
+            });
+        })
+
+        prevButton.addEventListener('click', () => {
+            const currentScroll = carouselContainer.scrollLeft;
+            let targetScroll = 0;
+            
+        for (let i = productCards.length - 1; i >= 0; i--) {
+            const card = productCards[i];
+
+            if (card.offsetLeft < currentScroll - (cardWidth / 2)) {
+                targetScroll = card.offsetLeft;
+                console.log("Gidilen konum", targetScroll);
+                break;
+            }
+        }
+            carouselContainer.scrollTo({
+                left: targetScroll,
+                behavior: 'smooth',
+            })
+        })
+    }
+});
